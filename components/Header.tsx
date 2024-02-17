e3c0a8a3de7bd04c62d914d820e92dcb3db7cf57
james <webdevuk1@gmail.com>
2024-02-17 10:32:12 +0000

    Added support for dark and light mode functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,17 @@
+import DarkModeToggle from "./DarkModeToggle";
 import Logo from "./Logo";
+import UserButton from "./UserButton";
 
 function Header() {
   return (
-    <header>
-      <nav>
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
+
+        <div className="flex-1 flex items-center justify-end space-x-4">
+          <DarkModeToggle />
+          <UserButton />
+        </div>
       </nav>
     </header>
   );
